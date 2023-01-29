9b0be73208c2d0974ebc4e09b5ecab806c1cb050
Neylix <julien.leclerc05@protonmail.com>
2023-01-29 13:15:53 +0100

    Add Ctrl-f mapping for hop

--- after/plugin/hop.lua
+++ after/plugin/hop.lua
@@ -13,6 +13,10 @@ vim.keymap.set('', 'F', function()
 	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
 end, { remap = true })
 
+vim.keymap.set('', '<C-f>', function()
+	hop.hint_char1({ direction = directions.AFTER_CURSOR })
+end, { remap = true })
+
 vim.keymap.set('', 't', function()
 	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
 end, { remap = true })
